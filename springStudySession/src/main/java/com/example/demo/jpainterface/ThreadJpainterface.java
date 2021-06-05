8b96d83c24eb42a556229bbe2a24bd77f771e2d8
YumaAsanae <gw.asanae.yuma@gmail.com>
2021-06-05 23:41:42 +0900

    Revert "insert Push a project for a study session."
    This reverts commit eb05da19f43ad6146d21fc09277ba045a1de8656.

--- springStudySession/src/main/java/com/example/demo/jpainterface/ThreadJpainterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.jpainterface;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.ThreadEntity;
-
-public interface ThreadJpainterface extends JpaRepository<ThreadEntity, Integer> {
-
-}
